b9346a0fd01e718051c741f8dc91c33ca0bd7208
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-02 22:23:21 +0530

    added rootElement

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-const element = document.querySelector('#root')
-ReactDOM.render(<App />, element)
+const rootElement = document.querySelector('#root')
+ReactDOM.render(<App />, rootElement)
