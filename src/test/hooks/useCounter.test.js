804f0151df14c0a12f9d2c21ab4ddcc00d2d3157
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-02 22:22:39 +0530

    added console

--- src/test/hooks/useCounter.test.js
+++ src/test/hooks/useCounter.test.js
@@ -2,7 +2,8 @@ import { renderHook } from '@testing-library/react-hooks';
 import { useCounter } from '../hooks/useCounter.js';
 
 it('Should have initial value of 0', () => {
-    const { result: { current } } = renderHook(() => useCounter());
+  const { result: { current } } = renderHook(() => useCounter());
+  console.log('test')
     
   expect(current.count).toEqual(0);
 });
