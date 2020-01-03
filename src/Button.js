ee903773589952345171561aff35c02a7cc6a109
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-03 20:43:09 +0530

    refector

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = props => {
     const { handleClick } = props;
-    // console.log("Button Component rendered")
+    // console.log("Button Component rendered:")
     return (
         <>
             <button className='btn' onClick={handleClick}>{props.children}</button>
