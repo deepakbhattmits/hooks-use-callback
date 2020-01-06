1b53c99416c29b2a1eacc2ec954f279e3f71f6f7
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-06 22:15:59 +0530

    refector added button

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,17 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
 
 const Button = props => {
-    const { handleClick } = props;
-    // console.log("Button Component rendered:")
-    return (
-        <>
-            <button className='btn' onClick={handleClick}>{props.children}</button>
-        </>
-    )
-}
+	const { handleClick } = props;
+	console.log('Button Component rendered:');
+	return (
+		<>
+			<button className='btn' onClick={handleClick}>
+				{props.children}
+			</button>
+		</>
+	);
+};
 
-export default React.memo(Button)
+export default React.memo(Button);
