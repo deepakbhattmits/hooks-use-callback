68016d59fa35c9fff171aac4a9284f289967fdec
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-06 22:14:32 +0530

    refector added textbox

--- src/Textbox.js
+++ src/Textbox.js
@@ -1,13 +1,20 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
 
 const Textbox = props => {
-    const { name, handleClick } = props
-    // console.log("Textbox :> Rendered")
-    return (
-        <div>
-            <input type="text" placeholder="Enter Name" value={name} onChange={handleClick} />
-        </div>
-    )
-}
+	const { name, handleClick } = props;
+	console.log('Textbox :> Rendered');
+	return (
+		<div>
+			<input
+				type='text'
+				placeholder='Enter Name'
+				value={name}
+				onChange={handleClick}
+			/>
+		</div>
+	);
+};
 
-export default React.memo(Textbox)
+export default React.memo(Textbox);
