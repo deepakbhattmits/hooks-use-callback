9c0e50c62f6e66e14c94d0fae986993d74229a4d
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-06 22:16:20 +0530

    refector component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Parent from './Parent'
+/** @format */
+
+import React from 'react';
+import Parent from './Parent';
 const App = () => {
-    console.log('props:')
-    return (
-        <div className='ui container grid'>
-            <Parent />
-        </div>
-    );
-}
+	// console.log('props:')
+	return (
+		<div className='ui container grid'>
+			<Parent />
+		</div>
+	);
+};
 export default App;
