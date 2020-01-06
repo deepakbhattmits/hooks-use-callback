26704bcd95de9602e6904da047ea2a62b1ba7970
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-06 22:14:17 +0530

    refector added hooks

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,14 +1,18 @@
-import React, { useState, useCallback } from 'react'
+/** @format */
 
-export const useCounter=( )=> {
-    const [count, setCount] = useState(0)
-    const incrementCounter = useCallback(() => {
-        setCount(count + 1)
-    }, [count])
-    const decrementCounter = useCallback(() => {
-        if (count > 0) {
-            setCount(count - 1)
-        }
-    }, [count])
-    return { count, incrementCounter, decrementCounter }
-}
+import React, { useState, useCallback } from 'react';
+
+export const useCounter = () => {
+	const [count, setCount] = useState(0);
+	const incrementCounter = useCallback(() => {
+		// console.log(count);
+		setCount(count + 1);
+	}, [count]);
+	const decrementCounter = useCallback(() => {
+		// console.log(count);
+		if (count > 0) {
+			setCount(count - 1);
+		}
+	}, [count]);
+	return { count, incrementCounter, decrementCounter };
+};
