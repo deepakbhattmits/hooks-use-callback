1ce398985c890f9dc883c7a2dda2aa47ab6c8418
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-09 20:04:32 +0530

    update

--- src/Title.js
+++ src/Title.js
@@ -1,13 +1,10 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
 
 const Title = () => {
-    // console.log("Title component rendered :>")
-    return (
+	// console.log("Title component rendered :")
+	return <div>React Title</div>;
+};
 
-        <div>
-            React Title
-        </div>
-    )
-}
-
-export default React.memo(Title)
+export default React.memo(Title);
