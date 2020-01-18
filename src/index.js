bb9106f0ca74e1c66dc601c9f24cc84a2b18db77
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-18 12:00:05 +0530

    refector

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
-const rootElement = document.querySelector('#root')
-ReactDOM.render(<App />, rootElement)
+/** @format */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+const rootElement = document.querySelector('#root');
+//main entry point
+ReactDOM.render(<App />, rootElement);
