82ea5f0c402471d1943dc48902465cbaec5af47e
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-18 14:27:58 +0530

    refector

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 const rootElement = document.querySelector('#root');
-//main entry point
+//main entry point of project
 ReactDOM.render(<App />, rootElement);
